09577e7421c82e44d28b38a5a1fb594b827b6731
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-02-21 17:50:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,29 @@ module.exports = function solveEquation(equation) {
     var a = arr[0];
     var b = (arr[3] == '-') ? -arr[4] : arr[4];
     var c = (arr[7] == '-') ? -arr[8] : arr[8];
+    
     var d = (b * b) - (4 * a * c);
+    var root1 = (-b + Math.sqrt(d)) / (2 * a);
+    var root2 = (-b - Math.sqrt(d)) / (2 * a);
+    root1 = Math.round(root1 * 10) / 10;
+    root2 = Math.round(root2 * 10) / 10;
+    if (root1 < root2) return [root1, root2];
+    else return [root2, root1];
+    /*
     switch(d) {
         case d < 0: {
             return [];
         }
         break;
         case d == 0: {
-            return [(-b + Math.sqrt(d)) / (2 * a)];
+            var root = (-b + Math.sqrt(d)) / (2 * a);
+            root = Math.round(root * 10) / 10;
+            return [root];
         }
         break;
         default: {
-            var root1 = (-b + Math.sqrt(d)) / (2 * a);
-            var root2 = (-b - Math.sqrt(d)) / (2 * a);
-            root1 = Math.round(root1 * 10) / 10;
-            root2 = Math.round(root2 * 10) / 10;
-            if (root1 < root2) return [root1, root2];
-            else return [root2, root1];
+            
         }
     }
+    */
 };
